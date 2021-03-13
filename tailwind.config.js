26b642969e3e89ae825a3e58c5b1edac142ada5e
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2021-03-13 19:26:21 +0530

    Confrigration added
    added various colours and confrigation which will be used as variables now

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        body:['Spartan']
+      },
+      colors:{
+        darkmagenta:'#502050',
+        softpink:'#EE68a4',
+        darkgraymagenta:'#937b92',
+        lightgraymagenta:'#f7f2f7',
+        white:'#ffffff',
+      },
+      backgroundimage:{
+        'hero-lg': "url('/images/bg-pattern-top-mobile.svg')",
+        'lower-side': "url('/images/bg-pattern-bottom-desktop.svg')",
+        'upper-side': "url('/images/bg-pattern-top-desktop.svg')",
+      },
+
+    },
   },
   variants: {
     extend: {},
